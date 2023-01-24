f39a0fac3ce1323b001a3efe655288d382fe13c3
BoyCho <53038672+BoyCho@users.noreply.github.com>
2023-01-24 22:52:57 +0900

    [level 2] Title: 오픈채팅방, Time: 164.55 ms, Memory: 39 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42888. 오픈채팅방/오픈채팅방.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <sstream>
+#include <cstring>
+#include <vector>
+#include <map>
+
+using namespace std;
+
+vector<string> solution(vector<string> record) {
+    
+    vector<pair<bool,string>> ch;
+    map<string,string> name;
+    vector<string> answer;
+    
+    for(int i = 0; i < record.size(); i++){
+        stringstream  ss(record[i]);
+        vector<string> tmp;
+        string s;
+        
+        while(getline(ss, s, ' ')) tmp.push_back(s);
+        
+        if (strcmp(tmp[0].c_str(),"Enter") == 0){
+            ch.push_back({true, tmp[1]});
+            name[tmp[1]] = tmp[2];
+        }
+        if (strcmp(tmp[0].c_str(),"Leave") == 0)
+            ch.push_back({false, tmp[1]});
+        else
+            name[tmp[1]] = tmp[2];
+    }
+    
+    for(int i = 0; i < ch.size(); i++){
+        if(ch[i].first)
+            answer.push_back(name[ch[i].second] + "님이 들어왔습니다.");
+        else
+            answer.push_back(name[ch[i].second] + "님이 나갔습니다.");
+    }
+    
+    
+    return answer;
+}
