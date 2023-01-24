ca878a6c5c2a55a6ee9f97317dcf7fdc428e79c6
BoyCho <53038672+BoyCho@users.noreply.github.com>
2023-01-24 22:52:25 +0900

    [level 1] Title: 실패율, Time: 0.01 ms, Memory: 3.68 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/42889. 실패율/실패율.cpp
@@ -0,0 +1,46 @@
+#include <string>
+#include <vector>
+#include <map>
+#include <queue>
+#include <algorithm>
+#include <iostream>
+
+using namespace std;
+
+
+struct cmp{
+    bool operator()(pair<double,int> a, pair<double,int> b){
+        if(a.first == b.first){
+            return a.second > b.second;
+        }
+        return a.first < b.first;
+    }
+};
+priority_queue<pair<double,int>, vector<pair<double,int>>, cmp> pq;
+map<int,int> m;
+
+vector<int> solution(int N, vector<int> stages) {
+    vector<int> answer;
+    double size = stages.size();
+    int max = 0;
+    
+    cout << "a";
+    for(int i = 0; i< size;i++){
+        m[stages[i]]++;
+        if(stages[i]>max) max = stages[i];
+    }
+    for(int i = 1; i <= N; i++){
+        double tmp = m[i];
+        if(size == 0 && tmp == 0){
+            pq.push({0,i});
+            continue;
+        }
+        pq.push({tmp/size, i});
+        size -= m[i];
+    }
+    while(!pq.empty()){
+        auto cur = pq.top(); pq.pop();
+        answer.push_back(cur.second);
+    }
+    return answer;
+}
