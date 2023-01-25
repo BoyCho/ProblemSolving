eeaa54a8e8e8ce1c926e71c122c607121a6f30db
BoyCho <53038672+BoyCho@users.noreply.github.com>
2023-01-25 21:36:12 +0900

    [level 2] Title: 후보키, Time: 0.63 ms, Memory: 75.1 MB -BaekjoonHub

--- 프로그래머스/lv2/42890. 후보키/후보키.java
+++ 프로그래머스/lv2/42890. 후보키/후보키.java
@@ -1,75 +1,42 @@
 import java.util.ArrayList;
-import java.util.List;
+import java.util.HashSet;
+import java.util.Set;
 
 public class Solution {
-
-    static List<List<Integer>> combList;
-    static List<List<Integer>> candKeyList;
-    static boolean[] vis;
-    static int attribute, tuple;
+    
+    ArrayList<Integer> cKeyList;
+    int n, m;
 
     public int solution(String[][] relation) {
-        tuple = relation.length;
-        attribute = relation[0].length;
-        combList = new ArrayList<>();
-        candKeyList = new ArrayList<>();
-        vis = new boolean[attribute];
 
-        for (int i = 1; i <= attribute; i++)
-            combination(0, i);
-
-        for (List<Integer> comb : combList) {
-            // 최소성 검사
-            if(checkMinimality(comb)) continue;
-            // 유일성 검사
-            checkUniqueness(relation, comb);
-        }
-        return candKeyList.size();
-    }
-
-    private boolean checkMinimality(List<Integer> comb) {
-        for (List<Integer> candKey : candKeyList) {
-            int cnt = 0;
-            for (int index : candKey) {
-                if (comb.contains(index))
-                    cnt++;
-            }
-            if (cnt == candKey.size())
-                return true;
-        }
-        return false;
-    }
-
-    private void checkUniqueness(String[][] relation, List<Integer> comb) {
-        for (int x = 0; x < tuple; x++) {
-            for (int y = 0; y < tuple; y++) {
-                if (x == y) continue;
-                int cnt = 0;
-
-                for (int index: comb) {
-                    if (relation[x][index].equals(relation[y][index]))
-                        cnt++;
-                    if (cnt == comb.size())
-                        return;
+        cKeyList = new ArrayList<>();   // 후보키 리스트
+        m = relation[0].length;         // m개의 컬럼
+        n = relation.length;            // n개의 데이터
+        
+        // 완전 탐색
+        for(int i = 1; i < (1<<m); i++) {
+            Set<String> checkUnique = new HashSet<>();
+            // n개의 데이터
+            for (int j = 0; j < n; j++) {
+                StringBuilder sb = new StringBuilder();
+                // m개의 컬럼
+                for (int k = 0; k < m; k++) {
+                    // 모든 경우의 수 & 해당 컬럼 조합 : 부분 집합 선택
+                    if ((i & (1 << k)) > 0)
+                        sb.append(relation[j][k]);
                 }
+                checkUnique.add(sb.toString());
             }
+            // 유일성 확인
+            if (checkUnique.size() != n) continue;
+            // 최소성 확인
+            checkMin(i);
         }
-        candKeyList.add(comb);
+        return cKeyList.size();
     }
-
-    void combination(int st, int r) {
-        if (r == 0) {
-            List<Integer> a = new ArrayList<>();
-            for (int i = 0; i < attribute; i++) {
-                if (vis[i]) a.add(i);
-            }
-            combList.add(a);
-            return;
-        }
-        for (int i = st; i < attribute; i++) {
-            vis[i] = true;
-            combination(i + 1, r - 1);
-            vis[i] = false;
-        }
+    public void checkMin(int i) {
+        for (Integer cKey : cKeyList)
+            if ((cKey & i) == cKey) return;
+        cKeyList.add(i);
     }
 }
