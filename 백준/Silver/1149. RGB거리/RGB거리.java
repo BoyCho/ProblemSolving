a3cfdcb3222e6e6a4076e4a92f3e6732581adb23
BoyCho <53038672+BoyCho@users.noreply.github.com>
2023-01-30 13:10:56 +0900

    [Silver I] Title: RGB거리, Time: 272 ms, Memory: 21056 KB -BaekjoonHub

--- 백준/Silver/1149. RGB거리/RGB거리.java
+++ 백준/Silver/1149. RGB거리/RGB거리.java
@@ -1,29 +1,31 @@
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int[][] cost = new int[n][3];
+public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int n = sc.nextInt();
+    int[][] cost = new int[n][3];
 
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < 3; j++) {
-                cost[i][j] = sc.nextInt();
-            }
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < 3; j++) {
+            cost[i][j] = sc.nextInt();
         }
-
-        int[][] dp = new int[n][3];
-        dp[0][0] = cost[0][0];
-        dp[0][1] = cost[0][1];
-        dp[0][2] = cost[0][2];
-
-        for (int i = 1; i < n; i++) {
-            dp[i][0] = Math.min(dp[i-1][1], dp[i-1][2]) + cost[i][0];
-            dp[i][1] = Math.min(dp[i-1][0], dp[i-1][2]) + cost[i][1];
-            dp[i][2] = Math.min(dp[i-1][0], dp[i-1][1]) + cost[i][2];
-        }
-
-        int answer = Math.min(dp[n-1][0], Math.min(dp[n-1][1], dp[n-1][2]));
-        System.out.println(answer);
     }
+
+    int[] dp = new int[3];
+    dp[0] = cost[0][0];
+    dp[1] = cost[0][1];
+    dp[2] = cost[0][2];
+
+    for (int i = 1; i < n; i++) {
+        int[] temp = new int[3];
+        temp[0] = Math.min(dp[1], dp[2]) + cost[i][0];
+        temp[1] = Math.min(dp[0], dp[2]) + cost[i][1];
+        temp[2] = Math.min(dp[0], dp[1]) + cost[i][2];
+        dp = temp;
+    }
+
+    int answer = Math.min(dp[0], Math.min(dp[1], dp[2]));
+    System.out.println(answer);
+}
 }
