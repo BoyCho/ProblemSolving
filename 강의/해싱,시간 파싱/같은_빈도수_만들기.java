3dab3c357232af63370ce30649faeaef32612eed
손현조 <iijokv@naver.com>
2023-03-29 17:51:29 +0900

    hashing2

--- 강의/해싱,시간 파싱/같은_빈도수_만들기.java
+++ 강의/해싱,시간 파싱/같은_빈도수_만들기.java
@@ -1,8 +1,23 @@
 import java.util.*;
 class 같은_빈도수_만들기 {
     public int[] solution(String s){
+        HashMap<Character, Integer> sH = new HashMap<>();
+        int max = Integer.MIN_VALUE;
+
+        for (char c : s.toCharArray())
+            sH.put(c, sH.getOrDefault(c, 0) + 1);
+
+        String tmp = "abcde";
+
+        for (char c : tmp.toCharArray()) {
+            if (sH.getOrDefault(c, 0) > max)
+                max = sH.get(c);
+        }
+
         int[] answer = new int[5];
 
+        for (int i = 0; i < 5; i++)
+            answer[i] = max - sH.getOrDefault(tmp.charAt(i), 0);
         return answer;
     }
 
