7e1a3ed34e8c3eb71c07f2cf835b42425031a8ac
손현조 <iijokv@naver.com>
2023-03-29 20:54:44 +0900

    hashing4

--- 강의/해싱,시간 파싱/음수가_있는_부분수열.java
+++ 강의/해싱,시간 파싱/음수가_있는_부분수열.java
@@ -1,7 +1,18 @@
+import java.util.*;
+
 class 음수가_있는_부분수열 {
     public int solution(int[] nums, int m){
-        int answer = 0;
+        HashMap<Integer, Integer> nH = new HashMap<>();
+        int sum = 0, answer = 0;
 
+        for (int n : nums) {
+            sum += n;
+
+            if (sum == m) answer++;
+            if (nH.get(sum - m) != null) answer += nH.get(sum - m);
+
+            nH.put(sum, nH.getOrDefault(sum, 0) + 1);
+        }
         return answer;
     }
 
