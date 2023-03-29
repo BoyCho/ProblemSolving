c743bfc0a2d8fed42a108bcf1923fceebe102ae1
손현조 <iijokv@naver.com>
2023-03-30 00:07:30 +0900

    hashing5

--- 강의/해싱,시간 파싱/회장선거.java
+++ 강의/해싱,시간 파싱/회장선거.java
@@ -1,8 +1,36 @@
+import java.util.*;
+
 class 회장선거 {
     public String solution(String[] votes, int k){
         String answer = " ";
+        Map<String, ArrayList<String>> presVote = new HashMap<>();
+        Map<String, Integer> presentMap = new HashMap<>();
+        Set<String> presSet = new HashSet<>();
 
+        for (String s : votes) {
+            String pres = s.split(" ")[1];
+            String voter = s.split(" ")[0];
 
+            presVote.putIfAbsent(pres, new ArrayList<>());
+            presVote.get(pres).add(voter);
+            presSet.add(pres);
+        }
+
+        for (String pres : presSet) {
+            if (presVote.get(pres).size() < k) continue;
+            for (String voter : presVote.get(pres))
+                presentMap.put(voter, presentMap.getOrDefault(voter, 0) + 1);
+        }
+
+        int max = 0;
+        for (Map.Entry<String, Integer> entry : presentMap.entrySet()) {
+            if (entry.getValue() == max)
+                answer = entry.getKey().compareTo(answer) < 0 ? entry.getKey() : answer;
+            if (entry.getValue() > max) {
+                answer = entry.getKey();
+                max = entry.getValue();
+            }
+        }
         return answer;
     }
 
