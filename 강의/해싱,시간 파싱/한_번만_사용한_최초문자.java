d213efdbf3b377e709044ba367dc726b62a1882e
손현조 <iijokv@naver.com>
2023-03-29 17:15:40 +0900

    hashing1

--- 강의/해싱,시간 파싱/한_번만_사용한_최초문자.java
+++ 강의/해싱,시간 파싱/한_번만_사용한_최초문자.java
@@ -1,8 +1,17 @@
+import java.util.*;
+
 class 한_번만_사용한_최초문자 {
     public int solution(String s){
-        int answer = 0;
+        Map<Character,Integer> map = new HashMap<>();
 
-        return answer;
+        for (int i = 0; i < s.length(); i++)
+            map.put(s.charAt(i), map.getOrDefault(s.charAt(i), 0) + 1);
+
+        for (int i = 0; i < s.length(); i++) {
+            if (map.get(s.charAt(i)) == 1)
+                return i + 1;
+        }
+        return -1;
     }
 
     public static void main(String[] args){
