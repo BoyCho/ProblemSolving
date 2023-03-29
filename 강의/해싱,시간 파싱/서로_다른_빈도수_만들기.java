d9be762d242518f0c530e04ce8ef8b63774051d3
손현조 <iijokv@naver.com>
2023-03-29 20:05:29 +0900

    hashing3

--- 강의/해싱,시간 파싱/서로_다른_빈도수_만들기.java
+++ 강의/해싱,시간 파싱/서로_다른_빈도수_만들기.java
@@ -1,6 +1,21 @@
+import java.util.*;
+
 class 서로_다른_빈도수_만들기 {
     public int solution(String s){
+        HashMap<Character, Integer> sH = new HashMap<>();
+        HashSet<Integer> set = new HashSet<>();
         int answer = 0;
+
+        for (char key : s.toCharArray())
+            sH.put(key, sH.getOrDefault(key, 0) + 1);
+
+        for (int freq : sH.values()) {
+            while (set.contains(freq) && freq != 0) {
+                freq--;
+                answer++;
+            }
+            if (freq > 0) set.add(freq);
+        }
 
         return answer;
     }
