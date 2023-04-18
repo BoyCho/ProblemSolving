e1ae9fd37d09c92856268f00baa2eecb9524a996
BoyCho <53038672+BoyCho@users.noreply.github.com>
2023-04-18 19:48:49 +0900

    [Silver II] Title: 나무 자르기, Time: 1084 ms, Memory: 119516 KB -BaekjoonHub

---
+++ 백준/Silver/2805. 나무 자르기/나무 자르기.java
@@ -0,0 +1,45 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static int n, m;
+    static int[] trees;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        trees = new int[n];
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++)
+            trees[i] = Integer.parseInt(st.nextToken());
+        Arrays.sort(trees);
+
+        int l = 0, r = trees[n-1] + 1;
+        while (l < r) {
+            int mid = (l + r) / 2;
+
+            if (getTrees(mid) < m)
+                r = mid;
+            else
+                l = mid + 1;
+        }
+        System.out.println(r - 1);
+    }
+
+    static long getTrees(int len) {
+        long res = 0;
+        for (int tree: trees) {
+            if (tree <= len) continue;
+            res += tree - len;
+        }
+        return res;
+    }
+}
