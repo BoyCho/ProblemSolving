65a38491f39f29adcb87d91ae86df592a23f6224
BoyCho <53038672+BoyCho@users.noreply.github.com>
2023-08-11 10:52:00 +0900

    [Gold V] Title: 치킨 배달, Time: 256 ms, Memory: 17772 KB -BaekjoonHub

--- 백준/Gold/15686. 치킨 배달/치킨 배달.java
+++ 백준/Gold/15686. 치킨 배달/치킨 배달.java
@@ -2,56 +2,64 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-
-    static List<int[]> H, S;
-    static int[] vis;
-    static int N, M, answer;
-
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-
-        N = Integer.parseInt(st.nextToken());
-        M = Integer.parseInt(st.nextToken());
-
-        H = new ArrayList<>();  // 집
-        S = new ArrayList<>();  // 치킨 가게
-
-        for (int i = 0; i < N; i++) {
-            st = new StringTokenizer(br.readLine());
-            for (int j = 0; j < N; j++) {
-                int v = Integer.parseInt(st.nextToken());
-
-                if (v == 1) H.add(new int[]{i, j});
-                if (v == 2) S.add(new int[]{i, j, 0});
-            }
-        }
-
-        vis = new int[S.size()];
-        answer = Integer.MAX_VALUE;
-
-        dfs(0,0);
-        System.out.println(answer);
-    }
-
-    static void dfs(int d, int s) {
-        if (d == M) {
-            int totalDist = 0;
-            for (int[] house: H) {
-                int minDist = Integer.MAX_VALUE;
-                for (int i = 0; i < S.size(); i++) {
-                    if (vis[i] == 0) continue;
-                    minDist = Math.min(minDist, Math.abs(S.get(i)[0] - house[0]) + Math.abs(S.get(i)[1] - house[1]));
-                }
-                totalDist += minDist;
-            }
-            answer = Math.min(answer, totalDist);
-            return;
-        }
-        for (int i = s; i < vis.length; i++) {
-            vis[i] = 1;
-            dfs(d + 1, i + 1);
-            vis[i] = 0;
-        }
-    }
+	
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static StringTokenizer stk;
+	
+	static List<int[]> H, C;
+	static int[] pick;
+	static int N, M, minD;
+	
+	static int[] dx = {0, 1, 0, -1};
+	static int[] dy = {1, 0, -1, 0};
+	
+	public static void main(String[] args) throws IOException {
+		stk = new StringTokenizer(br.readLine());
+		
+	    N = Integer.parseInt(stk.nextToken());
+	    M = Integer.parseInt(stk.nextToken());
+	    
+	    H = new ArrayList<>();
+	    C = new ArrayList<>();
+	    
+	    for (int i = 0; i < N; i++ ) {
+		    stk = new StringTokenizer(br.readLine());
+		    for (int j = 0; j < N; j++) {
+		    	int cur = Integer.parseInt(stk.nextToken());
+		    	
+		    	if (cur == 1) H.add(new int[] {i, j});
+		    	if (cur == 2) C.add(new int[] {i, j});
+		    }
+	    }
+	    
+	    minD = Integer.MAX_VALUE;
+	    pick = new int[M];
+	    
+	    comb(0, 0);
+	    
+	    System.out.println(minD);
+	}
+	
+	public static void comb(int L, int st) {
+		if (L == M) {
+			int totalD = 0;
+			for (int[] h : H) {
+				int min = Integer.MAX_VALUE;
+				for (int c : pick) 
+					min = Math.min(min, getDistance(h[0], h[1], C.get(c)[0], C.get(c)[1]));
+				totalD += min;
+			}
+			minD = Math.min(totalD, minD);
+			return;
+		}
+		
+		for (int i = st; i < C.size(); i++) {
+			pick[L] = i;
+			comb(L + 1, i + 1);
+		}
+	}
+	
+	public static int getDistance(int x, int y, int nx, int ny) {
+		return Math.abs(nx - x) + Math.abs(ny - y);
+	}
 }
