cfc3c3f0296f5998ba1bc7bb937058993d5e52f2
BoyCho <53038672+BoyCho@users.noreply.github.com>
2023-08-29 19:55:49 +0900

    [Silver I] Title: RGB거리, Time: 144 ms, Memory: 14576 KB -BaekjoonHub

--- 백준/Silver/1149. RGB거리/RGB거리.java
+++ 백준/Silver/1149. RGB거리/RGB거리.java
@@ -1,31 +1,48 @@
-import java.util.Scanner;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-public static void main(String[] args) {
-    Scanner sc = new Scanner(System.in);
-    int n = sc.nextInt();
-    int[][] cost = new int[n][3];
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer stk;
 
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < 3; j++) {
-            cost[i][j] = sc.nextInt();
+    static int N;
+    static int[][] d;
+    static int[] R, G ,B;
+
+    public static void main(String[] args) throws IOException {
+        N = Integer.parseInt(br.readLine());
+
+        d = new int[N][3];
+
+        R = new int[N];
+        G = new int[N];
+        B = new int[N];
+
+        /*
+        d[n][0] : n번 째 집이 빨강일 때의 최솟값
+        d[n][1] : n번 째 집이 초록일 때의 최솟값
+        d[n][2] : n번 째 집이 파랑일 때의 최솟값
+
+        d[n][0] = d[n-1][1] 과 d[n-1][2] 의 최솟값 + R[n]
+         */
+
+        for (int i = 0; i < N; i++) {
+            stk = new StringTokenizer(br.readLine());
+
+            R[i] = Integer.parseInt(stk.nextToken());
+            G[i] = Integer.parseInt(stk.nextToken());
+            B[i] = Integer.parseInt(stk.nextToken());
         }
+
+        d[0][0] = R[0];
+        d[0][1] = G[0];
+        d[0][2] = B[0];
+
+        for (int i = 1; i < N; i++) {
+            d[i][0] = Math.min(d[i - 1][1], d[i - 1][2]) + R[i];
+            d[i][1] = Math.min(d[i - 1][0], d[i - 1][2]) + G[i];
+            d[i][2] = Math.min(d[i - 1][0], d[i - 1][1]) + B[i];
+        }
+        System.out.println(Math.min(Math.min(d[N - 1][0], d[N - 1][1]), Math.min(d[N - 1][1], d[N - 1][2])));
     }
-
-    int[] dp = new int[3];
-    dp[0] = cost[0][0];
-    dp[1] = cost[0][1];
-    dp[2] = cost[0][2];
-
-    for (int i = 1; i < n; i++) {
-        int[] temp = new int[3];
-        temp[0] = Math.min(dp[1], dp[2]) + cost[i][0];
-        temp[1] = Math.min(dp[0], dp[2]) + cost[i][1];
-        temp[2] = Math.min(dp[0], dp[1]) + cost[i][2];
-        dp = temp;
-    }
-
-    int answer = Math.min(dp[0], Math.min(dp[1], dp[2]));
-    System.out.println(answer);
-}
 }
