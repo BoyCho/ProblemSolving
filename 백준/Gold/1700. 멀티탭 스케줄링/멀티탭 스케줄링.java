65136ddb77a270f5de6e9d4d58a309bc5ffea748
BoyCho <53038672+BoyCho@users.noreply.github.com>
2024-02-05 23:21:04 +0900

    [Gold I] Title: 멀티탭 스케줄링, Time: 80 ms, Memory: 11564 KB -BaekjoonHub

---
+++ 백준/Gold/1700. 멀티탭 스케줄링/멀티탭 스케줄링.java
@@ -0,0 +1,56 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+
+        int[] order = new int[K];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < K; i++) {
+            order[i] = Integer.parseInt(st.nextToken());
+        }
+
+        boolean[] use = new boolean[101];
+        int put = 0;
+        int ans = 0;
+        for (int i = 0; i < K; i++) {
+            int temp = order[i];
+
+            if (!use[temp]) {
+                if (put < N) {
+                    use[temp] = true;
+                    put++;
+                } else {
+                    ArrayList<Integer> arrList = new ArrayList<>();
+                    for (int j = i; j < K; j++) {
+                        if (use[order[j]] && !arrList.contains(order[j])) {
+                            arrList.add(order[j]);
+                        }
+                    }
+
+                    if (arrList.size() != N) {
+                        for (int j = 0; j < use.length; j++) {
+                            if (use[j] && !arrList.contains(j)) {
+                                use[j] = false;
+                                break;
+                            }
+                        }
+                    } else {
+                        int remove = arrList.get(arrList.size() - 1);
+                        use[remove] = false;
+                    }
+
+                    use[temp] = true;
+                    ans++;
+                }
+            }
+        }
+        System.out.println(ans);
+    }
+}
