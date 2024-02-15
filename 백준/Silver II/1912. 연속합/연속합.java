c75eafdd42e23b1c88fe7f035dc3ded12d66478f
BoyCho <53038672+BoyCho@users.noreply.github.com>
2024-02-15 23:42:38 +0900

    [Silver II] Title: 연속합, Time: 248 ms, Memory: 39128 KB -BaekjoonHub

---
+++ 백준/Silver II/1912. 연속합/연속합.java
@@ -0,0 +1,41 @@
+import java.io.*;
+import java.util.StringTokenizer;
+ 
+public class Main {
+	
+	static int[] arr;
+	static Integer[] dp;
+	static int max;	
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer stk;
+		
+		int N = Integer.parseInt(br.readLine());
+		
+		arr = new int[N];
+		dp = new Integer[N];
+		
+		
+		stk = new StringTokenizer(br.readLine(), " ");
+		for(int i = 0; i < N; i++) {
+			arr[i] = Integer.parseInt(stk.nextToken());
+		}
+		
+		dp[0] = arr[0];
+		max = arr[0];
+		
+		recur(N - 1);
+		
+		System.out.println(max);
+	}
+	
+	static int recur(int N) {
+		if(dp[N] == null) {
+			dp[N] = Math.max(recur(N - 1) + arr[N], arr[N]);
+			max = Math.max(dp[N], max);
+		}
+		
+		return dp[N];
+	}
+}
